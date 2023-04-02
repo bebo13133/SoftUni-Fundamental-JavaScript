46194fdc64fce3fbfe8d498e317c004b13661638
bebo13133 <borislaviliev47@gmail.com>
2023-04-02 21:16:17 +0300

    modify

---
+++ Basic Syntax,loops - Exercise/rounding/rouding.js
@@ -0,0 +1,12 @@
+function rounding(num, precision ){
+    if (precision >15){
+        precision = 15;
+    }
+    console.log (parseFloat(num.toFixed(precision)))
+    
+    
+    
+    
+    
+    
+    }
